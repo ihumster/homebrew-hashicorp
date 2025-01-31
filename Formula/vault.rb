5da968deaa0eaeac80f7654ae0f91a2b48943c91
Nikitin <alexander@ihumster.ru>
2025-01-31 04:39:11 +0000

    Update formula version

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -1,30 +1,30 @@
 class Vault < Formula
   desc "Vault"
   homepage "https://www.vaultproject.io"
-  version "1.18.3"
+  version "1.18.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.3/vault_1.18.3_darwin_amd64.zip"
-    sha256 "2459ba9cb8123e9b177e7e5d0b7c893779593d141d5bca7d24e21ad9edccfb65"
+    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.4/vault_1.18.4_darwin_amd64.zip"
+    sha256 "ead7d631454b0c8e895ddce770558d6a40e2b6423616150a0ac53cf9c4bf1ef8"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.3/vault_1.18.3_darwin_arm64.zip"
-    sha256 "88c22964804781934da7192174c9fdd00e9973b57f1b0f27c255cf7dbe333bad"
+    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.4/vault_1.18.4_darwin_arm64.zip"
+    sha256 "e0098a3dda3712b8f1ba14930fe9fa4edc8c68a8c01b46565797ea5198b60b6e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.3/vault_1.18.3_linux_amd64.zip"
-    sha256 "405ec904a45c2261e2c091640fb805bf5904fd2fe8a991ebc58d2eb64f9a269e"
+    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.4/vault_1.18.4_linux_amd64.zip"
+    sha256 "bdde42660c14700e4d5275760d50c59ed637803cd9c7c12926a2e2e788097a45"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.3/vault_1.18.3_linux_arm.zip"
-    sha256 "816df690b9240cf50828331012081b4221da4eecf30e1ce4d85053113138aab7"
+    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.4/vault_1.18.4_linux_arm.zip"
+    sha256 "19ab068acb3b25c1d049405401b35b4083877190714bfbddeaadcbd4f26d23d2"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.3/vault_1.18.3_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/vault/1.18.4/vault_1.18.4_linux_arm64.zip"
     sha256 ""
   end
 
