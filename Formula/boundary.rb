0a857c9e805080d03031f38dfed9c0d3dfa66a2a
Nikitin <alexander@ihumster.ru>
2025-02-11 04:39:51 +0000

    Update formula version

--- Formula/boundary.rb
+++ Formula/boundary.rb
@@ -1,30 +1,30 @@
 class Boundary < Formula
   desc "Boundary"
   homepage "https://www.boundaryproject.io/"
-  version "0.18.2"
+  version "0.19.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/boundary/0.18.2/boundary_0.18.2_darwin_amd64.zip"
-    sha256 "21af2f85e5fea8741b833c7fd9bd1172f6108f4170471dcdc6c45c91402da99c"
+    url "https://hashicorp-releases.yandexcloud.net/boundary/0.19.0/boundary_0.19.0_darwin_amd64.zip"
+    sha256 "7440eb11a96f500785069e423225c2ca84f0726d668e33ef7b3e30a6bbdf05bc"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/boundary/0.18.2/boundary_0.18.2_darwin_arm64.zip"
-    sha256 "e96ee2fd6044c5820f794b97a21a149a4088ae65a7c2ee627f70f0b95efeb773"
+    url "https://hashicorp-releases.yandexcloud.net/boundary/0.19.0/boundary_0.19.0_darwin_arm64.zip"
+    sha256 "fef1c9f5f02c8cdb0394cfa5be9e99f0a05a53be095a6f55a498e25abb6f1991"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/boundary/0.18.2/boundary_0.18.2_linux_amd64.zip"
-    sha256 "2b52371623745eb862780ea965ea773052623028487443353e7d32b4e903a169"
+    url "https://hashicorp-releases.yandexcloud.net/boundary/0.19.0/boundary_0.19.0_linux_amd64.zip"
+    sha256 "38ca73d3fb419e6d6194be19aa6dd3f54dd7ec0151bf87de2deeb43510378020"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/boundary/0.18.2/boundary_0.18.2_linux_arm.zip"
-    sha256 "942f02f1ba0d2dcb0d232b7abdc53c8aa2269d172dd38c5d815dec37f8ef63c9"
+    url "https://hashicorp-releases.yandexcloud.net/boundary/0.19.0/boundary_0.19.0_linux_arm.zip"
+    sha256 "e0df9a9a7d2528e0ad315488651448dfcc1c6644c8c955c0b55658983bfba23c"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/boundary/0.18.2/boundary_0.18.2_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/boundary/0.19.0/boundary_0.19.0_linux_arm64.zip"
     sha256 ""
   end
 
