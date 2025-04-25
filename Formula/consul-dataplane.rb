650ad41f4ab92e3efc1199883a18032a7bad5493
Nikitin <alexander@ihumster.ru>
2025-04-25 04:44:29 +0000

    Update formula version

--- Formula/consul-dataplane.rb
+++ Formula/consul-dataplane.rb
@@ -1,30 +1,30 @@
 class ConsulDataplane < Formula
   desc "Consul Dataplane"
   homepage "https://github.com/hashicorp/consul-dataplane"
-  version "1.4.5"
+  version "1.4.6"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.5/consul-dataplane_1.4.5_darwin_amd64.zip"
-    sha256 "df1eebc8f4773086b5147af416a16ca73e3dad4ff96659028bcf4544857d5378"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.6/consul-dataplane_1.4.6_darwin_amd64.zip"
+    sha256 "813a42c582604e2c936c05aa58351ea2123fabb09aafb6eb667ce8b85cb4f366"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.5/consul-dataplane_1.4.5_darwin_arm64.zip"
-    sha256 "c9b067108875ae12248efa932b37943356733718819483e0ee4d352a05d6c8d3"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.6/consul-dataplane_1.4.6_darwin_arm64.zip"
+    sha256 "3f934e21c4bd4b51e589fd641a8bcbe4ddd27319f5a1c3a5419c4547adaf4e74"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.5/consul-dataplane_1.4.5_linux_amd64.zip"
-    sha256 "cd68add5ecdcab81f63d52b9d1ef7af4b3b56bb055189cffc911b7dec8be026b"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.6/consul-dataplane_1.4.6_linux_amd64.zip"
+    sha256 "be8e376198026004f4c87222f0ca1d84088afdbc711a72a348b2062774d310d7"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.5/consul-dataplane_1.4.5_linux_arm.zip"
-    sha256 "0ab413b7006d3a716401ea7078bbfb199a018414890325e7cca3bea22e6729ab"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.6/consul-dataplane_1.4.6_linux_arm.zip"
+    sha256 "76f89dc1f26823439b900956d397285416e2989a54af1ca758acba8547f295db"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.5/consul-dataplane_1.4.5_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.4.6/consul-dataplane_1.4.6_linux_arm64.zip"
     sha256 ""
   end
 
