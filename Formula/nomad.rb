ce4aa134ba805b82a3d71d21d2589dc9479b9769
Nikitin <alexander@ihumster.ru>
2025-05-14 04:44:26 +0000

    Update formula version

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -1,25 +1,25 @@
 class Nomad < Formula
   desc "Nomad"
   homepage "https://www.nomadproject.io/"
-  version "1.10.0"
+  version "1.10.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.0/nomad_1.10.0_darwin_amd64.zip"
-    sha256 "2a7c1f4f85da9d2bed657c9502f4560e57966695f6bcacd3309ada33fcbcb61c"
+    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.1/nomad_1.10.1_darwin_amd64.zip"
+    sha256 "b1485cff769121b786dba35806d7820f4dc4ac5df2e248236f707d2ed6741fcc"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.0/nomad_1.10.0_darwin_arm64.zip"
-    sha256 "c71886a7fae7d622c0c579248249a9a1b68bf6da2d652c0446867705a08db24e"
+    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.1/nomad_1.10.1_darwin_arm64.zip"
+    sha256 "3621b5b1f0a0a78f708aa16743f00bb822499fa0451677f685546bbfc5102bff"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.0/nomad_1.10.0_linux_amd64.zip"
-    sha256 "d0936673cfa026b87744d60ad21ba85db70fe792b0685bfce95ac06a98d30b9d"
+    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.1/nomad_1.10.1_linux_amd64.zip"
+    sha256 "cc1cffd95d81943d0a7f8abe7dc2b025a644512d1af253456267caebc1f38065"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.0/nomad_1.10.0_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/nomad/1.10.1/nomad_1.10.1_linux_arm64.zip"
     sha256 ""
   end
 
