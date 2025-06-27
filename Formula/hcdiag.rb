e5f00abb9b26582c1b52f329cedec4615ea4f607
Nikitin <alexander@ihumster.ru>
2025-06-27 04:50:20 +0000

    Update formula version

--- Formula/hcdiag.rb
+++ Formula/hcdiag.rb
@@ -1,30 +1,30 @@
 class Hcdiag < Formula
   desc "Hcdiag"
   homepage "https://github.com/hashicorp/hcdiag"
-  version "0.5.7"
+  version "0.5.8"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.7/hcdiag_0.5.7_darwin_amd64.zip"
-    sha256 "05023a09c793689ac1e942b21b83ef75d0de5c4df824bcf536b6f6d4855eaed1"
+    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.8/hcdiag_0.5.8_darwin_amd64.zip"
+    sha256 "21ae17f6cb55d65ba3ba1dc96f990e86f17a1fa976dba4e1220c66eaea12e2a9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.7/hcdiag_0.5.7_darwin_arm64.zip"
-    sha256 "de7973538fb73d6393887f65210de95d219009b1091e82e85aebbfaa594be0bb"
+    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.8/hcdiag_0.5.8_darwin_arm64.zip"
+    sha256 "48d52fd6eb86cf07fe68defec8fe44b60e2f49a17dc4154cd702e1113805b267"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.7/hcdiag_0.5.7_linux_amd64.zip"
-    sha256 "cacdb7de0b60ab7d7b2de01bacdce82316303aec3d54707ed363d6024e795687"
+    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.8/hcdiag_0.5.8_linux_amd64.zip"
+    sha256 "624d846c82895ae6912576be6f1dbb3e0408175d7f12b03303dbfbc076b54552"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.7/hcdiag_0.5.7_linux_arm.zip"
-    sha256 "47ba3c81411bf135fa0337b38ccca5a4586f02e6cb9de7113d7beae7a3ab21e9"
+    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.8/hcdiag_0.5.8_linux_arm.zip"
+    sha256 "e3326d6d3e6260bdd6c8fdb4db180c5599f81799a9c419c207091684caf4113d"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.7/hcdiag_0.5.7_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/hcdiag/0.5.8/hcdiag_0.5.8_linux_arm64.zip"
     sha256 ""
   end
 
