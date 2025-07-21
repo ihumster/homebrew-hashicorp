dc09cba7d5fed81a6d0689237070a0131aad07cb
Nikitin <alexander@ihumster.ru>
2025-07-21 05:00:34 +0000

    Update formula version

--- Formula/consul-dataplane.rb
+++ Formula/consul-dataplane.rb
@@ -1,30 +1,30 @@
 class ConsulDataplane < Formula
   desc "Consul Dataplane"
   homepage "https://github.com/hashicorp/consul-dataplane"
-  version "1.7.2"
+  version ""
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.7.2/consul-dataplane_1.7.2_darwin_amd64.zip"
-    sha256 "e963116896ba86f81ff83dda741146bdfd7716504b6565fa64f57b4a5fa5f7c5"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane//consul-dataplane__darwin_amd64.zip"
+    sha256 ""
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.7.2/consul-dataplane_1.7.2_darwin_arm64.zip"
-    sha256 "fb6d362e76ce6040515d8b71a5e41ea451f434cf354e8c147c1486cb1aff548d"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane//consul-dataplane__darwin_arm64.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.7.2/consul-dataplane_1.7.2_linux_amd64.zip"
-    sha256 "d8d139cc10daeb6cca2939d32ea02a8d47bfb33dcb2bb25dc30573507da6e599"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane//consul-dataplane__linux_amd64.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.7.2/consul-dataplane_1.7.2_linux_arm.zip"
-    sha256 "2a949b61407805b9c8a802aabcbcd03cfa6a7ec81d2c1f756a2949fc1db8d038"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane//consul-dataplane__linux_arm.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane/1.7.2/consul-dataplane_1.7.2_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/consul-dataplane//consul-dataplane__linux_arm64.zip"
     sha256 ""
   end
 
--- Formula/hc-install.rb
+++ Formula/hc-install.rb
@@ -1,30 +1,30 @@
 class HcInstall < Formula
   desc "hc-install CLI"
   homepage "https://github.com/hashicorp/hc-install"
-  version "0.9.2"
+  version ""
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/hc-install/0.9.2/hc-install_0.9.2_darwin_amd64.zip"
-    sha256 "7895a2f9ad63eca9b479ba23658f421b921c473acbefafc111ef4e604853fc83"
+    url "https://hashicorp-releases.yandexcloud.net/hc-install//hc-install__darwin_amd64.zip"
+    sha256 ""
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/hc-install/0.9.2/hc-install_0.9.2_darwin_arm64.zip"
-    sha256 "5eac68118f9aa0767cbd9b73146b52a415be21007f9e5e3ed898f99a15c38ba9"
+    url "https://hashicorp-releases.yandexcloud.net/hc-install//hc-install__darwin_arm64.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/hc-install/0.9.2/hc-install_0.9.2_linux_amd64.zip"
-    sha256 "dfbad402f9b82fca22e41de8bfba209ed353eafff8f0adb761cfd9a723031e67"
+    url "https://hashicorp-releases.yandexcloud.net/hc-install//hc-install__linux_amd64.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/hc-install/0.9.2/hc-install_0.9.2_linux_arm.zip"
-    sha256 "69dbfa35fd043bb4f307cf0d8c8df7f4b55f3d9f474e00ae1df9f1f62da9b15e"
+    url "https://hashicorp-releases.yandexcloud.net/hc-install//hc-install__linux_arm.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/hc-install/0.9.2/hc-install_0.9.2_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/hc-install//hc-install__linux_arm64.zip"
     sha256 ""
   end
 
