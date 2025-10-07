a5762dccb1f14c9245ae9411ad47b7a48972e147
Nikitin <alexander@ihumster.ru>
2025-10-07 04:41:29 +0000

    Update formula version

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -1,30 +1,30 @@
 class TerraformLs < Formula
   desc "Terraform Language Server"
   homepage "https://github.com/hashicorp/terraform-ls"
-  version "0.38.0"
+  version "0.38.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.0/terraform-ls_0.38.0_darwin_amd64.zip"
-    sha256 "75dab5c6c9cc4cdd5f0163bf64e4e0ab8c654eb266054f8bc453868dbbb61c41"
+    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.1/terraform-ls_0.38.1_darwin_amd64.zip"
+    sha256 "ccab0879e1227703765c8b1d3d7d99f716c8330e588bc62d105b962d427f9810"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.0/terraform-ls_0.38.0_darwin_arm64.zip"
-    sha256 "f689fa10342ae3b1c52b223592734f6d827da544ad593338253b63253fdfb51d"
+    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.1/terraform-ls_0.38.1_darwin_arm64.zip"
+    sha256 "c8430c498f722244eeb7c573d4d327113c617bd9911b06e461148446dd171b1d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.0/terraform-ls_0.38.0_linux_amd64.zip"
-    sha256 "4f9222705f0c1652de1753291863ac7b11fc8c3f4cc310f2806a5b56a6c79a71"
+    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.1/terraform-ls_0.38.1_linux_amd64.zip"
+    sha256 "c5869f946b0d17e2c13157dc3bc2aea87fab12d39ac17b3a198102332e79bf62"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.0/terraform-ls_0.38.0_linux_arm.zip"
-    sha256 "966bcf44e12c2e4a57e2d8ab5a4f3a1dbdec7adbe0054e53e191e283a5f68243"
+    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.1/terraform-ls_0.38.1_linux_arm.zip"
+    sha256 "df903934b1b097df446eaa9ffe0b7ae2947e27242c381fde12a92ec50264ac23"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.0/terraform-ls_0.38.0_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/terraform-ls/0.38.1/terraform-ls_0.38.1_linux_arm64.zip"
     sha256 ""
   end
 
