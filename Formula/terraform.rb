fd814b9bc44f0680610a9fb6434f5d065f3e5d92
Nikitin <alexander@ihumster.ru>
2025-10-16 04:41:59 +0000

    Update formula version

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,30 +1,30 @@
 class Terraform < Formula
   desc "Terraform"
   homepage "https://www.terraform.io/"
-  version "1.13.3"
+  version "1.13.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.3/terraform_1.13.3_darwin_amd64.zip"
-    sha256 "5ef8e19091106b1921af26db5bcee3cd84a475eae2fd190fc02b1049b320d042"
+    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.4/terraform_1.13.4_darwin_amd64.zip"
+    sha256 "0a4441f6aaf37bb18cbaf5c7538f9be638f0d93eb325d285b4b452d014a7737a"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.3/terraform_1.13.3_darwin_arm64.zip"
-    sha256 "8362e7284b38a1194884963deed83481696d468b42dab88052775f4280383584"
+    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.4/terraform_1.13.4_darwin_arm64.zip"
+    sha256 "6b9e2ff8102c469added7be28cd5cfc2c5a8f7fc3be2df8fe9ee5458d84a1305"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.3/terraform_1.13.3_linux_amd64.zip"
-    sha256 "71fc43d92ea09907be5d416d2405a6a9c2d1ceaed633f5e175c0af26e8c4b365"
+    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.4/terraform_1.13.4_linux_amd64.zip"
+    sha256 "98aa516201e948306698efd9954ab4cc0d1227c2578ba56245898b5f679e590b"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.3/terraform_1.13.3_linux_arm.zip"
-    sha256 "fa82fb1b08354573467557f33e6a15e7f9e1bba74eb15492f151ca27525d2acc"
+    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.4/terraform_1.13.4_linux_arm.zip"
+    sha256 "a17bde150a4d6c9e7ece063ab634c07723b8242e078f3ae9017486277d6690c4"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.3/terraform_1.13.3_linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/terraform/1.13.4/terraform_1.13.4_linux_arm64.zip"
     sha256 ""
   end
 
