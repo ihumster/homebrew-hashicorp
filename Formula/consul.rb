f93927a0489bb192f7d82bab4e1fb8e465e024dd
Nikitin <alexander@ihumster.ru>
2025-12-09 04:48:03 +0000

    Update formula version

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -1,30 +1,30 @@
 class Consul < Formula
   desc "Consul"
   homepage "https://www.consul.io"
-  version ""
+  version "1.22.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul//consul__darwin_amd64.zip"
-    sha256 ""
+    url "https://hashicorp-releases.yandexcloud.net/consul/1.22.1/consul_1.22.1_darwin_amd64.zip"
+    sha256 "1d8f48639618b8d293e3cfe7855df9fb53c802262848053bf45c260fd7427493"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/consul//consul__darwin_arm64.zip"
-    sha256 ""
+    url "https://hashicorp-releases.yandexcloud.net/consul/1.22.1/consul_1.22.1_darwin_arm64.zip"
+    sha256 "1bbbdc43ac23321f402d661b20dd0d291047ecbda2dbfe93e4b4ce26cf2c701d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul//consul__linux_amd64.zip"
-    sha256 ""
+    url "https://hashicorp-releases.yandexcloud.net/consul/1.22.1/consul_1.22.1_linux_amd64.zip"
+    sha256 "91222c7ec141f1c2c92f6b732eeb0251220337e4c07c768cbc6ae633fef69733"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul//consul__linux_arm.zip"
-    sha256 ""
+    url "https://hashicorp-releases.yandexcloud.net/consul/1.22.1/consul_1.22.1_linux_arm.zip"
+    sha256 "d421c4537b57935a949b9bc34252266672fe484a766a05ec5826bbae16c8f323"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul//consul__linux_arm64.zip"
+    url "https://hashicorp-releases.yandexcloud.net/consul/1.22.1/consul_1.22.1_linux_arm64.zip"
     sha256 ""
   end
 
