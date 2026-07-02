cfb881443de4cd580133f75457d1163ff8d67238
Nikitin <alexander@ihumster.ru>
2026-07-02 07:49:25 +0000

    Update formula version

--- Formula/consul-k8s.rb
+++ Formula/consul-k8s.rb
@@ -1,31 +1,31 @@
 class ConsulK8s < Formula
   desc "Consul K8s"
   homepage "https://github.com/hashicorp/consul-k8s"
-  version "1.8.14"
+  version "2.0.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/1.8.14/consul-k8s_1.8.14_darwin_amd64.zip"
-    sha256 "c364aa878b5fd12036385347469f24404eca9c2eb77f7155e0ec2e3804d77c89"
+    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/2.0.1/consul-k8s_2.0.1_darwin_amd64.zip"
+    sha256 "c0a5e9a8a37598289512cf57a8cd4d4fad976cb373acfbcc2c0268c95d1c2e30"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/1.8.14/consul-k8s_1.8.14_darwin_arm64.zip"
-    sha256 "e6eb1ce68472e5ed8bc8b74388853b209e95fae33aac227cae1ce4884a364693"
+    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/2.0.1/consul-k8s_2.0.1_darwin_arm64.zip"
+    sha256 "02dcf84c48823e70fc0a55e048456b22fcfa3ddb860647b40d24836ae8e7a5db"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/1.8.14/consul-k8s_1.8.14_linux_amd64.zip"
-    sha256 "d6f9f77ec9a7a5595ebb619036c23023ef0f2799d6a158e1c5a79cfb7d8e83b7"
+    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/2.0.1/consul-k8s_2.0.1_linux_amd64.zip"
+    sha256 "e988e4d3e24387df35ae10f50948faab32434a5cce80fa233ae8512e806271c0"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/1.8.14/consul-k8s_1.8.14_linux_arm.zip"
-    sha256 "a821e38aab87b0e748dfb81e2584abda6c7d81dd1fd98e8b84c096698b70938c"
+    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/2.0.1/consul-k8s_2.0.1_linux_arm.zip"
+    sha256 "c0cedbce1584dd6a82f8bb66372bae8aacd9fdd6ec1becf9b260f3050511e34c"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/1.8.14/consul-k8s_1.8.14_linux_arm64.zip"
-    sha256 "67e0926b5b425ad6bf398b1d5b5b85bbaab99f1b63c4a884a651d4060ec1372f"
+    url "https://hashicorp-releases.yandexcloud.net/consul-k8s/2.0.1/consul-k8s_2.0.1_linux_arm64.zip"
+    sha256 "53f6b1fc34a8818a68f0f17061aca80bdb703beaf39cb6c6f7375e4b0845beb0"
   end
 
   conflicts_with "consul-k8s"
